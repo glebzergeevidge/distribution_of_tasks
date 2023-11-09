d7828c36e53f1203d60b147c8363e784d659cf54
Gleb <138581652+glebzergeevidge@users.noreply.github.com>
2023-11-09 21:27:53 +0300

    Finally project1_v3.py

--- project1_v3.py
+++ project1_v3.py
@@ -1,11 +1,14 @@
 #читаем файл работников 
-employees = open('employees.txt', encoding='utf-8', mode='r').read().split('\n')
+file_employees = open('employees.txt', encoding='utf-8', mode='r')
+employees = file_employees.read().split('\n')
 
 
 #читаем файл задач
-tasks = open('tasks.txt', encoding='utf-8', mode='r').read().split('\n')
+file_tasks = open('tasks.txt', encoding='utf-8', mode='r')
+tasks = file_tasks.read().split('\n')
 
 
+#создаем список с уровнями
 levels = ['junior', 'middle', 'senior']
 
 
@@ -37,14 +40,14 @@ for i in range(len(words_tasks)):
 
 #скилы задач и время на их выполнение
 skills_tasks = []
-hours = []
-hour = []
+hours = [] #для вывода в конце
+hour = [] #для подсчета суммы часов
 for i in range(len(words_tasks)):
     for j in range(len(words_tasks[i])):
         if words_tasks[i][j] in levels:
             skills_tasks.append(' '.join(words_tasks[i][j + 1:-1]))
-            hours.append(int(tasks[i][-1]))
-            hour.append(int(tasks[i][-1]))
+            hours.append(int(tasks[i].split()[-1]))
+            hour.append(int(tasks[i].split()[-1]))
 
 
 #имя работника и скилы работника
@@ -92,46 +95,50 @@ for i in range(len(tasks)):
         names_tasks.append(tasks[i].split('senior')[0])
 
 
+#функция для проверки соответствия скилов задач и работника
+def contain(a, b):
+    c = 0
+    for i in a:
+        if i not in b:
+            c = 1
+    if c == 1:
+        return 0
+    return 1
+
+
 #сумма часов
 sum_hours = [0] * len(employees)
 sum_hour = 0
 for i in range(len(employees)):
     for j in range(len(tasks)):
-        if level_employees[i] >= level_tasks[j] and skills_tasks[j] in skills_employees[i]:
+        if level_employees[i] >= level_tasks[j] and contain(skills_tasks[j].split(), skills_employees[i].split()) == 1:
+
             sum_hour += hour[j]
             hour[j] = 0
-            
-            if sum_hour != 0:
-                sum_hours[i] = sum_hour
+
+            if sum_hour == 0:
+                sum_hours[i] = 0 
             else:
-                sum_hours[i] = 0    
-
-        elif level_employees[i] <= level_tasks[j] and skills_tasks[j] not in skills_employees[i]:
-                sum_hour = 0
-
+                sum_hours[i] = sum_hour
+    sum_hour = 0
+      
 
 #проверка на соответсвие уровня и скилов работника и задачи, запись списка задач в файл
 can = []
 cannot = [] 
-plans = open('plans.txt', encoding='utf-8', mode='w+')
+plans = open('plans.txt', encoding='utf-8', mode='w+')       
 for i in range(len(employees)):
-    if sum_hours[i] > 0:
-        plans.write(f'{names_employees[i]} - {sum_hours[i]}\n')
+    plans.write(f'{names_employees[i]} - {sum_hours[i]}\n')
+    if sum_hours[i] == 0:
+        plans.write('| Пока на расслабоне, на чиле |\n')
     k = 0
     for j in range(len(tasks)):  
-        
-        if level_employees[i] >= level_tasks[j] and skills_tasks[j] in skills_employees[i]:
-            
-            can.append(names_tasks[j])                                  
+        if level_employees[i] >= level_tasks[j] and contain(skills_tasks[j].split(), skills_employees[i].split()) == 1:
+            can.append(names_tasks[j])
             k += 1
             plans.write(f'{k}. {names_tasks[j]}- {hours[j]}\n')
-            skills_tasks[j] = '    '
-
-            
-        elif level_employees[i] <= level_tasks[j] and skills_tasks[j] not in skills_employees[i]:
-            c = 0
-    plans.write('в\n')
-        
+            skills_tasks[j] = 'Распределено'     
+    plans.write('\n')
 
                 
 #задачи, которые никто не может выполнить     
@@ -142,20 +149,14 @@ for i in range(len(names_tasks)):
 
 #запись задач, которые никто не может решить в файл
 plans.write('Задачи, которые никто не может решить:\n')
-for i in range(len(cannot)):
-    plans.write(f'{i + 1}. {cannot[i]} \n')
+if len(cannot) > 0:
+    for i in range(len(cannot)):
+        plans.write(f'{i + 1}. {cannot[i]} \n')
+else:
+    plans.write('| Таких задач нет |')
 
 
-
-
-                
-                
-
-
-
-        
-
-
-
-
-
+#закрытие всех файлов
+file_employees.close()
+file_tasks.close()
+plans.close()
